3b80295b9238cbfb939cfeae3ad16ae9a98fe724
lichen404 <f360family@gmail.com>
2021-08-16 02:24:50 +0800

    利用 connect 减少重复渲染

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useState, useContext, createContext, FC} from 'react';
 
 type Action<T = any> = {
@@ -11,15 +11,52 @@ type User = {
     name: string,
     age: number
 }
+type Store<T = any> = {
+    state: T,
+    setState: (newState: T) => void,
+    listeners: ((arg: any) => void)[],
+    subscribe: (fn: (arg: any) => void) => () => void
+}
 
-const User: FC = () => {
-    const contextValue = useContext<any>(appContext);
-    return <div>User:{contextValue.appState.user.name}</div>;
+const connect: <T = any>(Component: FC<Props<T>>) => FC<any> = (Component) => {
+    return (props) => {
+        const [, update] = useState({});
+        useEffect(() => {
+            store.subscribe(() => {
+                update({});
+            });
+        }, []);
+        const {state, setState} = useContext(appContext);
+        const dispatch = (action: Action<User>) => {
+            setState(reducer(state, action));
+            update({});
+        };
+        return <Component dispatch={dispatch} state={state} {...props}/>;
+    };
 };
+
+const User: FC = connect<User>(({state}) => {
+    return <div>User:{state.name}</div>;
+});
 
 const appContext = createContext<any>(null);
 
 
+const store: Store<User> = {
+    state: {name: 'frank', age: 18},
+    setState(newState) {
+        store.state = newState;
+        store.listeners.map(fn => fn(store.state));
+    },
+    listeners: [],
+    subscribe(fn) {
+        store.listeners.push(fn);
+        return () => {
+            const index = store.listeners.indexOf(fn);
+            store.listeners.splice(index, 1);
+        };
+    }
+};
 
 const reducer: Reducer<User> = (state, {type, payload}) => {
     if (type === 'updateUser') {
@@ -37,17 +74,11 @@ type Props<T = any> = {
     [key: string]: any
 }
 
-const connect: <T = any>(Component: FC<Props<T>>) => FC<any> = (Component) => {
-    const {appState, setAppState} = useContext(appContext);
-    const dispatch = (action: Action<User>) => {
-        setAppState(reducer(appState, action));
-    };
-    return (props) => <Component dispatch={dispatch} state={appState} {...props}/>;
-};
+
 
 const UserModifier = connect<User>(({dispatch, state}) => {
     return (<div>
-        <input type="text" value={state.name} onChange={(e) => {
+        <input type="text" value={state.name || ''} onChange={(e) => {
             dispatch({
                 type: 'updateUser',
                 payload: {name: e.target.value}
@@ -57,19 +88,24 @@ const UserModifier = connect<User>(({dispatch, state}) => {
     </div>);
 });
 
-const FirstChild: FC = () => <section>大儿子<User/></section>;
+const FirstChild: FC = () => {
+    console.log('大儿子执行了');
+    return <section>大儿子<User/></section>;
+};
 
-const SecondChild: FC = () => <section>二儿子<UserModifier/></section>;
+const SecondChild: FC = () => {
+    console.log('二儿子执行了');
+    return <section>二儿子<UserModifier/></section>;
+};
 
-const ThirdChild: FC = () => <section>三儿子</section>;
+const ThirdChild: FC = () => {
+    console.log('三儿子执行了');
+    return <section>三儿子</section>;
+};
 
 function App() {
-    const [appState, setAppState] = useState({
-        user: {name: 'frank', age: 18}
-    });
-    const contextValue = {appState, setAppState};
     return (
-        <appContext.Provider value={contextValue}>
+        <appContext.Provider value={store}>
             <FirstChild/>
             <SecondChild/>
             <ThirdChild/>
