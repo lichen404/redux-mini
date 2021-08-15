6bb21a8ba767adf316e183f237b2be39666ad7ab
lichen404 <f360family@gmail.com>
2021-08-16 00:16:21 +0800

    创建 reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import {useState, useContext, createContext, FC} from 'react';
 
+
+type Reducer<T> = (state: T, action: { type: string, payload: Partial<T> }) => T
+
+type User = {
+    name: string,
+    age: number
+}
+
+const User: FC = () => {
+    const contextValue = useContext<any>(appContext);
+    return <div>User:{contextValue.appState.user.name}</div>;
+};
+
 const appContext = createContext<any>(null);
 
 const FirstChild: FC = () => <section>大儿子<User/></section>;
@@ -9,20 +22,32 @@ const SecondChild: FC = () => <section>二儿子<UserModifier/></section>;
 
 const ThirdChild: FC = () => <section>三儿子</section>;
 
+
+const reducer: Reducer<User> = (state, {type, payload}) => {
+    if (type === 'updateUser') {
+        return {
+            ...state,
+            ...payload
+        };
+    } else {
+        return state;
+    }
+};
 const UserModifier: FC = () => {
-    const contextValue = useContext(appContext);
-    return <div>
-        <input type="text" value={contextValue.appState.user.name} onChange={(e) => {
-            contextValue.appState.user.name = e.target.value;
-            contextValue.setAppState({...contextValue.appState});
+    const {appState, setAppState} = useContext(appContext);
+    return (<div>
+        <input type="text" value={appState.user.name} onChange={(e) => {
+            const newState = reducer(appState, {
+                    type: 'updateUser',
+                    payload: {name: e.target.value}
+                }
+            );
+            setAppState(newState)
+            ;
         }}/>
-    </div>;
+    </div>);
 };
 
-const User: FC = () => {
-    const contextValue = useContext<any>(appContext);
-    return <div>User:{contextValue.appState.user.name}</div>;
-};
 
 function App() {
     const [appState, setAppState] = useState({
